b3a7e0969d7bb74132bef6076463f9d768ca0647
ryuheimat <ryuheimat3@gmail.com>
2021-01-18 04:56:15 +0900

    test limitations

--- test/2_dai-lp-pool-mock.test.js
+++ test/2_dai-lp-pool-mock.test.js
@@ -39,12 +39,7 @@ contract('DaiLpPool Mainnet testing', (accounts) => {
   let daiDebaseLp;
   let lpSupply;
 
-  let deposits = [
-    {
-      lp: (new BN('10')).mul((new BN('10')).pow(decimals))
-    }    
-  ]
-  let stakingReward = (new BN('100000')).mul((new BN('10')).pow(decimals));
+  let lastDepositId = 0;
 
   before('Get contract references', async () => {
     dai = await TestERC20.new((new BN('10000000')).mul((new BN('10')).pow(decimals)).toString(), "Test dai", "DAI");
@@ -66,8 +61,6 @@ contract('DaiLpPool Mainnet testing', (accounts) => {
       {from: accounts[0]}
     );
 
-    await dai.transfer(mphStaking.address, stakingReward.toString(), {from: accounts[0]});
-  
     uniFactory = await UniswapV2Factory.new(treasury, {from: accounts[0]});
     uniRouter02 = await UniswapV2Router02.new(uniFactory.address, config.weth, {from: accounts[0]});
 
@@ -88,6 +81,7 @@ contract('DaiLpPool Mainnet testing', (accounts) => {
     const lpAddress = await uniFactory.getPair(dai.address, debase.address);
     daiDebaseLp = await IERC20.at(lpAddress);
     lpSupply = new BN((await daiDebaseLp.totalSupply()).toString());
+
     daiLpPool = await DaiLpPool.new(
       daiDebaseLp.address,
       dai.address,
@@ -106,75 +100,127 @@ contract('DaiLpPool Mainnet testing', (accounts) => {
     await daiLpPool.setMphFee(20, {from: policy});
   });
 
-  describe('Test single deposit', () => {
-    it('Deposit', async () => {
-      const daiTotal = new BN((await dai.balanceOf(daiDebaseLp.address)).toString());
-      const debaseTotal = new BN((await debase.balanceOf(daiDebaseLp.address)).toString());
-      await daiDebaseLp.transfer(accounts[1], deposits[0].lp, {from: accounts[0]});
-      await daiDebaseLp.approve(daiLpPool.address, deposits[0].lp, {from: accounts[1]});
-      await daiLpPool.deposit(deposits[0].lp, {from: accounts[1]});
-      assert.equal((await daiDebaseLp.totalSupply()).toString(), (lpSupply.sub(deposits[0].lp)).toString());
-      assert.equal((await daiLpPool.lpDeposits(accounts[1])).toString(), deposits[0].lp.toString());    
-      assert.equal((await daiLpPool.totalLpLocked()).toString(), deposits[0].lp.toString());
-      deposits[0].dai = daiTotal.sub(new BN((await dai.balanceOf(daiDebaseLp.address)).toString()));
-      deposits[0].debase = debaseTotal.sub(new BN((await debase.balanceOf(daiDebaseLp.address)).toString()));
-      assert.equal((await dai.balanceOf(dInterest.address)).toString(), deposits[0].dai.toString());
-      assert.equal((await dai.balanceOf(daiLpPool.address)).toString(), 0);
-  
-      let depositInfo = await daiLpPool.deposits(0);
-      assert.equal(depositInfo.owner, accounts[1]);
-      assert.equal(depositInfo.amount.toString(), deposits[0].lp.toString());
-      assert.equal(depositInfo.daiAmount.toString(), deposits[0].dai.toString());
-      // assert.equal(depositInfo.daiAmount, deposits[0].debase);
-      assert.equal(depositInfo.daiDepositId, 1);
-      assert.equal(depositInfo.mphReward.toString(), "0");
-      // assert.equal(depositInfo.mphReward.toString(), deposits[0].dai.div(new BN('2')).toString());
-      assert.equal(depositInfo.mphVestingIdx, 0);
-      assert.equal(depositInfo.withdrawed, false);
+  describe('Test limitations', async () => {
+    let deposit = (new BN('10')).mul((new BN('10')).pow(decimals));
+    before('Send lp to account', async() => {
+      await daiDebaseLp.transfer(accounts[8], deposit, {from: accounts[0]});
+      await daiDebaseLp.approve(daiLpPool.address, deposit, {from: accounts[8]});
+    });
 
-      const mphReward = deposits[0].dai.div(new BN('2'));
-      assert.equal((await mph.balanceOf(vesting.address)).toString(), mphReward.toString());
-
-      deposits[0].daiInterest = (new BN('100')).mul((new BN('10')).pow(decimals));
-      deposits[0].daiStakingReward = (new BN('50')).mul((new BN('10')).pow(decimals));
-      await dInterest.setNextInterest(deposits[0].daiInterest);
-      await mphStaking.setNextReward(deposits[0].daiStakingReward);
-
+    it('enable/disable pools', async () => {
+      await daiLpPool.setPoolEnabled(false);
+      await expectRevert(
+        daiLpPool.deposit(deposit, {from: accounts[8]}),
+        "Pool isn't enabled"
+      );
+      await daiLpPool.setPoolEnabled(true);
+      await daiLpPool.deposit(deposit, {from: accounts[8]});
+      lastDepositId += 1;
+      await daiLpPool.setPoolEnabled(false);
       await time.increase(config.lockPeriod);
-      await daiLpPool.withdraw(0, 0, {from: accounts[1]});
+      await expectRevert(
+        daiLpPool.withdraw(0, 0, {from: accounts[8]}),
+        "Pool isn't enabled"
+      );
+      await daiLpPool.setPoolEnabled(true);
+      await daiLpPool.withdraw(0, 0, {from: accounts[8]});
+    });
 
-      deposits[0].daiStakingReward = deposits[0].daiStakingReward
-        .mul(new BN('1000000000000'))
-        .div(mphReward)
-        .mul(mphReward)
-        .div(new BN('1000000000000'))
-      depositInfo = await daiLpPool.deposits(0);
-      assert.equal(depositInfo.owner, accounts[1]);
-      assert.equal(depositInfo.amount.toString(), deposits[0].lp.toString());
-      assert.equal(depositInfo.daiAmount.toString(), deposits[0].dai.toString());
-      assert.equal(depositInfo.daiDepositId, 1);
-      assert.equal(depositInfo.mphReward.toString(), mphReward.toString());
-      assert.equal(depositInfo.mphVestingIdx, 0);
-      assert.equal(depositInfo.withdrawed, true);
+    it('enable/disable total lp limit', async () => {
+      await daiLpPool.setTotalLpLimitEnabled(true);
+      await daiLpPool.setTotalLpLimit((new BN('9')).mul((new BN('10')).pow(decimals)));
+      await expectRevert(
+        daiLpPool.deposit(deposit, {from: accounts[8]}),
+        "To much lp locked"
+      );
+      await daiLpPool.setTotalLpLimitEnabled(false);
+    });
+    it('enable/disable total lp limit', async () => {
+      await daiLpPool.setMaxDepositLimitEnabled(true);
+      await daiLpPool.setMaxDepositLimit((new BN('9')).mul((new BN('10')).pow(decimals)));
+      await expectRevert(
+        daiLpPool.deposit(deposit, {from: accounts[8]}),
+        "to much deposit for this user"
+      );
+      await daiLpPool.setMaxDepositLimitEnabled(false);
+    });
+  });
 
-      const mphRewardRemain = mphReward.sub(mphReward.mul(new BN('30')).div(new BN('100')));
-      const mphFee = mphRewardRemain.mul(new BN('2')).div(new BN('100'));
+  describe('Test single deposit', async () => {
+    before('enable pool', async () => {
+      await daiLpPool.setPoolEnabled(true);
+    });
+    it('Deposit', async () => {
+      let deposits = [
+        {lp: (new BN('10')).mul((new BN('10')).pow(decimals))},
+        {lp: (new BN('15')).mul((new BN('10')).pow(decimals))},
+        {lp: (new BN('7')).mul((new BN('10')).pow(decimals))},
+      ]
+      for (let i = 0; i < deposits.length; i += 1) {
+        lpSupply = new BN((await daiDebaseLp.totalSupply()).toString());
+        let daiTotal = new BN((await dai.balanceOf(daiDebaseLp.address)).toString());
+        let debaseTotal = new BN((await debase.balanceOf(daiDebaseLp.address)).toString());
+        await daiDebaseLp.transfer(accounts[i + 1], deposits[i].lp, {from: accounts[0]});
+        await daiDebaseLp.approve(daiLpPool.address, deposits[i].lp, {from: accounts[i + 1]});
+        await daiLpPool.deposit(deposits[i].lp, {from: accounts[i + 1]});
+        assert.equal((await daiDebaseLp.totalSupply()).toString(), (lpSupply.sub(deposits[i].lp)).toString());
+        assert.equal((await daiLpPool.lpDeposits(accounts[i + 1])).toString(), deposits[i].lp.toString());    
+        assert.equal((await daiLpPool.totalLpLocked()).toString(), deposits[i].lp.toString());
+        deposits[i].dai = daiTotal.sub(new BN((await dai.balanceOf(daiDebaseLp.address)).toString()));
+        deposits[i].debase = debaseTotal.sub(new BN((await debase.balanceOf(daiDebaseLp.address)).toString()));
 
-      const mphBal1 = new BN((await mph.balanceOf(accounts[1])).toString());
-      const mphBal2 = new BN((await mph.balanceOf(treasury)).toString());
+        let depositInfo = await daiLpPool.deposits(lastDepositId + i);
+        assert.equal(depositInfo.owner, accounts[i + 1]);
+        assert.equal(depositInfo.amount.toString(), deposits[i].lp.toString());
+        assert.equal(depositInfo.daiAmount.toString(), deposits[i].dai.toString());
+        // assert.equal(depositInfo.daiAmount, deposits[i].debase);
+        assert.equal(depositInfo.daiDepositId, lastDepositId + i + 1);
+        assert.equal(depositInfo.mphReward.toString(), "0");
+        // assert.equal(depositInfo.mphReward.toString(), deposits[i].dai.div(new BN('2')).toString());
+        assert.equal(depositInfo.mphVestingIdx, lastDepositId + i);
+        assert.equal(depositInfo.withdrawed, false);
 
-      assert.equal(mphBal2.toString(), mphFee.toString());
-      assert.equal((mphBal1.add(mphBal2)).toString(), mphRewardRemain.toString());
+        let mphReward = deposits[i].dai.div(new BN('2'));
+        assert.equal((await mph.balanceOf(vesting.address)).toString(), mphReward.toString());
 
-      assert.equal((await mph.balanceOf(accounts[1])).toString(), mphRewardRemain.sub(mphFee).toString());
-      assert.equal((await mph.balanceOf(treasury)).toString(), mphFee.toString());
+        deposits[i].daiInterest = (new BN('100')).mul((new BN('10')).pow(decimals));
+        deposits[i].daiStakingReward = (new BN('50')).mul((new BN('10')).pow(decimals));
+        await dInterest.setNextInterest(deposits[i].daiInterest);
+        await mphStaking.setNextReward(deposits[i].daiStakingReward);
 
-      const daiFee = deposits[0].daiInterest.add(deposits[0].daiStakingReward).mul(new BN('3')).div(new BN('100'));
-      assert.equal(
-        (await dai.balanceOf(accounts[1])).toString(),
-        deposits[0].dai.add(deposits[0].daiInterest).add(deposits[0].daiStakingReward).sub(daiFee).toString());
-      assert.equal(
-        (await dai.balanceOf(treasury)).toString(), daiFee.toString());
+        await time.increase(config.lockPeriod);
+        const oldMphBalance = new BN((await mph.balanceOf(treasury)).toString());
+        const oldDaiBalance = new BN((await dai.balanceOf(treasury)).toString());
+        await daiLpPool.withdraw(lastDepositId + i, 0, {from: accounts[i + 1]});
+
+        deposits[i].daiStakingReward = deposits[i].daiStakingReward
+          .mul(new BN('1000000000000'))
+          .div(mphReward)
+          .mul(mphReward)
+          .div(new BN('1000000000000'))
+        depositInfo = await daiLpPool.deposits(lastDepositId + i);
+        assert.equal(depositInfo.owner, accounts[i + 1]);
+        assert.equal(depositInfo.amount.toString(), deposits[i].lp.toString());
+        assert.equal(depositInfo.daiAmount.toString(), deposits[i].dai.toString());
+        assert.equal(depositInfo.daiDepositId, lastDepositId + i + 1);
+        assert.equal(depositInfo.mphReward.toString(), mphReward.toString());
+        assert.equal(depositInfo.mphVestingIdx, lastDepositId + i);
+        assert.equal(depositInfo.withdrawed, true);
+
+        let mphRewardRemain = mphReward.sub(mphReward.mul(new BN('30')).div(new BN('100')));
+        let mphFee = mphRewardRemain.mul(new BN('2')).div(new BN('100'));
+
+        assert.equal((await mph.balanceOf(accounts[i + 1])).toString(), mphRewardRemain.sub(mphFee).toString());
+
+        assert.equal((await mph.balanceOf(treasury)).toString(), oldMphBalance.add(mphFee).toString());
+
+        let daiFee = deposits[i].daiInterest.add(deposits[i].daiStakingReward).mul(new BN('3')).div(new BN('100'));
+        assert.equal(
+          (await dai.balanceOf(accounts[i + 1])).toString(),
+          deposits[i].dai.add(deposits[i].daiInterest).add(deposits[i].daiStakingReward).sub(daiFee).toString());
+        assert.equal(
+          (await dai.balanceOf(treasury)).toString(), oldDaiBalance.add(daiFee).toString());
+      }
     });
   });
 });
