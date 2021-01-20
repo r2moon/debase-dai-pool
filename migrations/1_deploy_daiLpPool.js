70d8e12f4c226e5c08a64837ca4d090837299edd
ryuheimat <ryuheimat3@gmail.com>
2021-01-20 21:21:48 +0900

    deploy script

---
+++ migrations/1_deploy_daiLpPool.js
@@ -0,0 +1,20 @@
+const DaiLpPool = artifacts.require("DaiLpPool");
+const config = require("../config.json");
+
+module.exports = async function (deployer) {
+  deployer.deploy(
+    DaiLpPool,
+    config.debaseDaiPair,
+    config.dai,
+    config.debase,
+    config.mph,
+    config.policy,
+    config.daiFixedPool,
+    config.mphStakingPool,
+    config.mphVesting,
+    config.lockPeriod,
+    config.treasury,
+    config.debaseRewardPercentage,
+    config.blockDuration
+  );
+};
