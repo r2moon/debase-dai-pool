8f67e026ba5d5f1f68348280c4de8bf470943db6
ryuheimat <ryuheimat3@gmail.com>
2021-03-18 05:00:22 +0900

    change mph vesting period

--- migrations/1_deploy_daiLpPool.js
+++ migrations/1_deploy_daiLpPool.js
@@ -2,19 +2,19 @@ const DaiLpPool = artifacts.require("DaiLpPool");
 const config = require("../config.json");
 
 module.exports = async function (deployer) {
-  deployer.deploy(
-    DaiLpPool,
-    config.debaseDaiPair,
-    config.dai,
-    config.debase,
-    config.mph,
-    config.policy,
-    config.daiFixedPool,
-    config.mphStakingPool,
-    config.mphVesting,
-    config.lockPeriod,
-    config.treasury,
-    config.debaseRewardPercentage,
-    config.blockDuration
-  );
+  // deployer.deploy(
+  //   DaiLpPool,
+  //   config.debaseDaiPair,
+  //   config.dai,
+  //   config.debase,
+  //   config.mph,
+  //   config.policy,
+  //   config.daiFixedPool,
+  //   config.mphStakingPool,
+  //   config.mphVesting,
+  //   config.lockPeriod,
+  //   config.treasury,
+  //   config.debaseRewardPercentage,
+  //   config.blockDuration
+  // );
 };
